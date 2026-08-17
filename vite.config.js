77f471c96e3a426e2c51096b00a1014969367d85
Nalathone <107338351+Nalathone@users.noreply.github.com>
2026-08-17 23:32:21 +0700

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages repository path
+  base: '/Calculator/',
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
